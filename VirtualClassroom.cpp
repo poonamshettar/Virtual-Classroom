fae098f065ae39467b80c7f9b98ee44eb35fce5d
AbhijeetJ-512 <01fe21bei045@kletech.ac.in>
2024-01-27 12:47:19 +0530

    Virtual class room basic code

---
+++ VirtualClassroom.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "VirtualClassroom.h"
+#include "UserAuthentication.h"
+
+using namespace std;
+void VirtualClassroom::run()
+{
+    int x;
+    while (1)
+    {
+        cout << "1)Log in\n2)Regsiter\n3)exit\n";
+        cin >> x;
+        switch (x)
+        {
+
+        case 1:
+        {
+            string username, password;
+            cout << "Enter username: ";
+            cin >> username;
+            cout << "Enter password: ";
+            cin >> password;
+
+            UserType userType = UserAuthentication::authenticate(username, password);
+            break;
+        }
+        case 2:
+        {
+            string username, password;
+            int userTypeInt;
+            cout << "Enter username: ";
+            cin >> username;
+            cout << "Enter password: ";
+            cin >> password;
+            cout << "Enter user type (0 for student, 1 for teacher): ";
+            cin >> userTypeInt;
+
+            UserType userType = static_cast<UserType>(userTypeInt);
+
+            UserAuthentication::registerUser(username, password, static_cast<UserType>(userType));
+            cout << "Registration successful! You can now log in." << endl;
+            break;
+        }
+        }
+    }
+}
---
+++ VirtualClassroom.h
@@ -0,0 +1,13 @@
+#ifndef VIRTUALCLASSROOM_H
+#define VIRTUALCLASSROOM_H
+
+class VirtualClassroom
+{
+public:
+    void run();
+
+private:
+    int fun;
+};
+
+#endif
