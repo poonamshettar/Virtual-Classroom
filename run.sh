da882ff8155003ef3526f6ff1e6708939f28d2cf
AbhijeetJ-512 <01fe21bei045@kletech.ac.in>
2024-01-28 12:35:33 +0530

    main file to run all code at once

---
+++ run.sh
@@ -0,0 +1,8 @@
+g++ -c User.cpp
+g++ -c Student.cpp
+g++ -c Teacher.cpp
+g++ -c UserAuthentication.cpp
+g++ -c VirtualClassroom.cpp
+g++ -c main.cpp
+
+g++ -o main main.o VirtualClassroom.o UserAuthentication.o User.o Student.o Teacher.o
